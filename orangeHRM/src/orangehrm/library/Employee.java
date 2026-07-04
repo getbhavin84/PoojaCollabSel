250fe6e4068a808bb1891ca506e67c25576f12c5
getbhavin84 <getbhavin84@gmail.com>
2026-07-05 00:10:11 +0530

    Final messege b4 repo upload

---
+++ orangeHRM/src/orangehrm/library/Employee.java
@@ -0,0 +1,56 @@
+package orangehrm.library;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import utils.AppUtils;
+
+public class Employee extends AppUtils
+{
+
+	
+	public boolean addEmployee(String fname,String lname)
+	{
+		
+		driver.findElement(By.linkText("PIM")).click();
+		driver.findElement(By.linkText("Add Employee")).click();
+		
+		driver.findElement(By.id("firstName")).sendKeys(fname);
+		driver.findElement(By.id("lastName")).sendKeys(lname);
+		
+		String empno = driver.findElement(By.id("employeeId")).getAttribute("value");
+		driver.findElement(By.id("btnSave")).click();
+		
+		driver.findElement(By.linkText("Employee List")).click();
+		driver.findElement(By.id("empsearch_id")).sendKeys(empno);
+		driver.findElement(By.id("searchBtn")).click();
+		
+		WebElement emptable;
+		emptable = driver.findElement(By.id("resultTable"));
+		
+		List<WebElement> rows,cols;
+		rows = emptable.findElements(By.tagName("tr"));
+		
+		boolean isemppresent = false;
+		
+		for(int i=1;i<rows.size();i++)
+		{
+			cols =  rows.get(i).findElements(By.tagName("td"));
+			if(cols.get(1).getText().equals(empno))
+			{
+				isemppresent = true;
+				break;				
+			}
+		}
+		return isemppresent;
+		
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ orangeHRM/src/orangehrm/testcases/AdminLoginTestwithInvalidData.java
@@ -0,0 +1,27 @@
+package orangehrm.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import orangehrm.library.LoginPage;
+import utils.AppUtils;
+
+public class AdminLoginTestwithInvalidData extends AppUtils 
+{
+
+	
+	@Parameters({"adminuid","adminpwd"})
+	@Test
+	public void checkAdminLogin(String uid,String pwd)
+	{
+		LoginPage lp = new LoginPage();
+		lp.login(uid, pwd);
+		boolean res =  lp.isErrMsgDisplayed();
+		Assert.assertTrue(res);		
+	}
+	
+	
+	
+	
+}
